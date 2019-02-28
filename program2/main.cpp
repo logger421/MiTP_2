bf36043dc70993be559352d38c8385df2ce336fc
logger421 <46714539+logger421@users.noreply.github.com>
2019-02-28 15:36:34 +0100

    Update main.cpp

--- program2/main.cpp
+++ program2/main.cpp
@@ -9,12 +9,12 @@ private:
     int srednia_waga;
     int srednia_wysokosc;
 public:
-    Psy(string r,int wg,int wys){
+    Psy(string r,int wg,int wys){ //konstrukor
     rasa=r;
     srednia_waga=wg;
     srednia_wysokosc=wys;
     }
-    void inf_o_psie(void);
+    void inf_o_psie(void); //metody in out
     void dodaj_psa();
 };
 int main(void)
@@ -22,14 +22,14 @@ int main(void)
     Psy p1("Dalmatynczyk",58,24);
     Psy p2("Owczarek szetlandzki",22,15);
     Psy p3("Owczarek niemiecki",30,20);
-    Psy p4("Nic",1,1);
-    p1.inf_o_psie();
+    Psy p4("Nic",1,1); //inicjalizujemy go zmiennymi unikamy dzieki temu problemow moze nie tutaj ale np w programach
+    p1.inf_o_psie();    //do liczenia moglibysmy przypadkiem podzielic przez 0
     p2.inf_o_psie();
     p3.dodaj_psa();
     p3.inf_o_psie();
     p4.dodaj_psa();
 }
-void Psy::inf_o_psie(void)
+void Psy::inf_o_psie(void) // 
 {
     cout<<endl<<"Rasa: "<<rasa<<endl;
     cout<<"Srednia waga: "<<srednia_waga<<" kg"<<endl;
